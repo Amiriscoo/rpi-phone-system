588a131ac654c706624015689a3baa09eb6a0e66
Amiriscoo <Amiriscoo@users.noreply.github.com>
2026-09-10 17:43:21 -0400

    Checkout fetched Bookworm pi-gen commit

--- os/build.sh
+++ os/build.sh
@@ -29,8 +29,7 @@ if [[ ! -d "$PI_GEN_DIR" ]]; then
     git clone --depth=1 --branch bookworm https://github.com/RPi-Distro/pi-gen.git "$PI_GEN_DIR"
 else
     git -C "$PI_GEN_DIR" fetch --depth=1 origin bookworm
-    git -C "$PI_GEN_DIR" checkout -q bookworm
-    git -C "$PI_GEN_DIR" reset --hard -q origin/bookworm
+    git -C "$PI_GEN_DIR" checkout -q -B bookworm FETCH_HEAD
 fi
 
 rm -rf "$PI_GEN_DIR/stage-rpi-phone"
